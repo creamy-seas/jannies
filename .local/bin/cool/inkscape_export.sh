9cec8d62037c8e47620671ca530d33cbee655314
Ilya Antonov <ilya.antonov@dreams-ai.com>
2019-12-14 20:58:12 +0800

    fixed inkscape

--- .local/bin/cool/inkscape_export.sh
+++ .local/bin/cool/inkscape_export.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# 1 - copy over to temp folder
 BASENAME=$(sed "s/\..*//" <<< $1)
-dir=$(pwd)
-BASENAME="$(pwd)/$BASENAME"
+cp $BASENAME.svg /tmp
+TEMPNAME=/tmp/$BASENAME
 
-echo $BASENAME
+# 2 - run inkscape export and trim
+inkscape --file=$TEMPNAME.svg --export-area-drawing --without-gui --export-png=$TEMPNAME.png 2> /dev/null
+convert $TEMPNAME.png -trim $TEMPNAME.png 2> /dev/null
 
-inkscape --file=$BASENAME".svg" --export-area-drawing --without-gui --export-png=$BASENAME"_temp.png" 2> /dev/null
-
-convert $BASENAME"_temp.png" -trim $BASENAME".png" 2> /dev/null
-rm $BASENAME"_temp.png"
+# 3 - copy to original folder
+mv $TEMPNAME.png .
+rm $TEMPNAME.svg
 echo "==> Completed"
